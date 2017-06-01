b94494162ac48e9ca26e06f70a0cc13eea17d4be
Egraz Marlene <marlene.egraz@gmail.com>
2017-06-01 13:40:54 +0200

    index et correction

--- index.php
+++ index.php
@@ -4,15 +4,35 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Document</title>
+   <title>Mon blog</title>
 </head>
 <body>
    <h1> Mon blog </h1>
-   <?php $log_directory = 'posts';$results_array = array();if (is_dir($log_directory))
+<?php 
+
+$files = scandir("posts");
+foreach($files as $file) {
+        if (is_dir($file)) {
+                continue;
+        }
+        echo '<h1>' .basename($file, ".txt"). '</h1>';
+        $content = file_get_contents('posts/' .$file);
+        echo '<p>' .$content. '</p>';
+}
+
+
+
+
+
+
+
+
+/*$log_directory = 'posts';$results_array = array();if (is_dir($log_directory))
 {
        if ($handle = opendir($log_directory))
        {
-              
+              //handle pointeur de systeme de fichier de type ressource
+              cree en utlisant la fonction fopen//
                while(($file = readdir($handle)) !== FALSE)
                {
                        $results_array[] = $file;
@@ -20,9 +40,13 @@
                closedir($handle);
        }
 }foreach($results_array as $value)
+//pour enlever les points qui st à l'ecran//
+if(is_file('posts/' .$value)) 
 {
    echo "<ul>".$value ."</ul>". '<br/>';
 }
+*/
+
 ?>
 </body>
 </html>
